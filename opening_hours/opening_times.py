48b46a6e1c29ed4c0bd8ac5c0e8c2590880a4783
Martin Filliau <martin@filliau.com>
2012-08-30 10:55:34 +0100

    Deal with the "off" value for one day."

--- opening_hours/opening_times.py
+++ opening_hours/opening_times.py
@@ -3,6 +3,7 @@ def is_open(time, value):
     Check if the given time is inside the given value.
     Return True if it is, else False.
     """
+    if value == "off": return False
     to_check = get_minutes_from_midnight(time)
     from_time, to_time = value.split('-')
     from_time = get_minutes_from_midnight(from_time)
