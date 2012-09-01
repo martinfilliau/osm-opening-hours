34f0ce3d893caf74ca18b860b17f188baa836ea2
Martin Filliau <martin@filliau.com>
2012-09-01 19:29:37 +0200

    Re-enabled test with real (but cleaned) data from oxfordshire.

--- tests/test_ox.py
+++ tests/test_ox.py
@@ -5,8 +5,10 @@ from opening_hours import OpeningHours
 class TestOxfordshireDataset(unittest.TestCase):
 
     def test_dataset(self):
-        pass
-        #lines = open("tests/dataset_oxfordshire_20120829").readlines()
-        #for line in lines:
-        #    oh = OpeningHours(line)
-        #    oh.is_open("Mo", "14:00")
+        # Test with an extract of opening_hours from OSM Oxfordshire
+        # Values known to be incorrect have been removed, you can see
+        # the complete file (vs. _valid) in the tests/ directory.
+        lines = open("tests/dataset_oxfordshire_20120829_valid").readlines()
+        for line in lines:
+            oh = OpeningHours(line)
+            oh.is_open("Mo", "14:00")
